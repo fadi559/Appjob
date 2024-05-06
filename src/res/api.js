d0ae430435bbd5b8e65f491025a88ab56ba3a60a
fadi <fadishqerat54@icloud.com>
2024-05-06 21:36:25 +0300

    b
     b

--- src/res/api.js
+++ src/res/api.js
@@ -1,5 +1,5 @@
 
-export const baseUrl = 'http://192.168.1.241:8000'
+export const baseUrl = 'https://fadinodejss.onrender.com'
 
 export const Api={
     signup:baseUrl + "/api/signup",
@@ -15,6 +15,7 @@ export const Api={
  } 
 // localhost
 // 192.168.1.241
+// 'http://192.168.1.241:8000'
 // 
 // 
 
